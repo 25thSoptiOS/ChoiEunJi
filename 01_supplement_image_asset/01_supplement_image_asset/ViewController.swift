e885dbdb26ad805c17be13ce9e374b7cb95159b5
ChoiEunji0114 <cheunji8209@naver.com>
2019-10-16 17:37:37 +0900

    Upload 01_supplement_image_asset

---
+++ 01_supplement_image_asset/01_supplement_image_asset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01_supplement_image_asset
+//
+//  Created by 최은지 on 16/10/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
