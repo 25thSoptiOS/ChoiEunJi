09d5e56ae7079f0191e9b9e634dbf97b35745c4b
ChoiEunji0114 <cheunji8209@naver.com>
2019-10-16 16:46:24 +0900

    upload 01_seminar_storyboard

---
+++ 01_seminar_storyboard/01_seminar_storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01_seminar_storyboard
+//
+//  Created by 최은지 on 16/10/2019.
+//  Copyright © 2019 최은지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
